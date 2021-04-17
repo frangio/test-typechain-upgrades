501d3ad0dfef91e5feb64bcf71b197cc032f84ef
Francisco Giordano <frangio.1@gmail.com>
2021-04-17 00:05:32 -0300

    add @openzeppelin/hardhat-upgrades with patch for typechain

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,8 @@ import "@typechain/hardhat";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
 
+import "@openzeppelin/hardhat-upgrades";
+
 import "./tasks/accounts";
 import "./tasks/clean";
 
